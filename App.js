ae44d2e011b2a2288a2cb7e49d2b2f6799033e2a
karinaperdomo <perdomokarina1910@gmail.com>
2022-11-03 10:22:45 +0100

    primeravista

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
+import App from './src/components/firstView';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Primer mensaje: Karina!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+
+
+
+
+
+
+export default App;
