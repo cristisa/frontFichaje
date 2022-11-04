6220b903e10f65a528779a434683e70c5c6b7a77
Roxana Rojas <roxana.rojas.condor@gmail.comgit config --global user.email roxana.rojas.condor@gmail.com>
2022-11-04 12:14:06 +0100

    cambiorox

--- components/colors.js
+++
@@ -1,4 +0,0 @@
-export const Colors ={
-    primary: '#5870F6',
-    secondary: '#DF000'
-}
---
+++ components/fichajito.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Text, View, StyleSheet, Image, Alert } from 'react-native';
+import { AppButton } from "./AppButton";
+
+
+export default function fichajito() {
+    return (
+        <View style={styles.container}>
+        <View style={styles.header}>
+          <Image style={styles.image}/>
+          <Text style={styles.headertext}>08:05 am</Text>
+        </View>
+
+        <View style={styles.pp}>
+          <View style={styles.containercomment}>
+            <Text style={styles.comment}>FICHAJE OLVIDADO</Text>
+          </View>
+
+          <AppButton 
+            onPress={() => Alert.alert('Comentario enviado.')}
+            title='Enviar'
+            styles={styles.btn}
+            color="#fff" />
+        </View>
+      </View>
+    );
+  }
+
+  
+  const styles = StyleSheet.create({
+    container:{
+      backgroundColor: '#D9D9D9',
+      width: '100%',
+      height: '100%',
+    },
+    header: {
+      backgroundColor: '#EB5629',
+      height: '20%',
+    },
+    image:{
+      width: 500,
+    },
+    headertext: {
+      textAlign: 'center',
+      fontSize: 40,
+      marginTop: 10,
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+    pp:{
+      backgroundColor: '#fff',
+      width: '80%',
+      height: '45%',
+      display: 'flex',
+      alignItems: 'center',
+      marginLeft: 40,
+      marginTop: 50,
+      borderRadius: 25,
+    },
+    containercomment:{
+      color: '#fff',
+      marginTop: 25,
+      width: '80%',
+    },
+    comment:{
+      fontSize: 18,
+      marginBottom: 10,
+      fontWeight: 'bold',
+    },
+    input:{
+      backgroundColor:'#EBEBEB',
+      borderColor: '#ADADAD',
+      borderWidth: 2,
+      borderRadius: 5,
+      height: 80,
+      textAlignVertical: 'top',
+    },
+    btn: {
+      backgroundColor: '#5870F6',
+      color: 'red',
+      borderRadius: 25,
+      paddingTop: 12,
+      paddingBottom: 12,
+      paddingLeft: 50,
+      paddingRight: 50,
+      marginTop: 90,
+    },
+})
