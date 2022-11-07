7aab84a933293b4e39ce0ab3f66999e24288e515
Cristina Sanjuan <cristy.si@gmail.com>
2022-11-07 18:01:04 +0100

    UserContext

--- .history/App_20221107174519.js
+++ .history/App_20221107174519.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigation from './src/Components/navigation/DrawerNavigator';
+import { UserContext } from './src/Components/context/UserContext'; 
 // import { MainStackNavigator } from './src/Components/navigation/StackNavigator';
-import { UserContextProvider } from './src/Components/context/UserContext';
 
 export default function App() {
   return (
-  <UserContextProvider>
+  <UserContext> 
     <NavigationContainer>
       <DrawerNavigation />
     </NavigationContainer>
-  </UserContextProvider>  
+  </UserContext> 
   );
 }
--- .history/App_20221107175814.js
+++ .history/App_20221107175814.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigation from './src/Components/navigation/DrawerNavigator';
+import {UserContext} from 'context/UserContext'; 
 // import { MainStackNavigator } from './src/Components/navigation/StackNavigator';
 import { UserContextProvider } from './src/Components/context/UserContext';
 
